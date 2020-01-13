b809e7aabae310950aaea3140f4af14043bb9fec
Ayouvix <47478403+Ayouvix@users.noreply.github.com>
2020-01-13 01:34:54 +0100

    Initial commit

---
+++ js/plugins.js
@@ -0,0 +1,26 @@
+/* Setting Interval time loop of images */
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval:4000
+    });
+});
+
+
+/* animation button go to top */
+var btn = $('#top-btn');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '2000');
+});
+ 
+
+/* event */
