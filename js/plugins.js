b460b782327c452bc9883f1012a3fb9c1fdca0c6
Ayouvix <47478403+Ayouvix@users.noreply.github.com>
2020-01-16 00:19:54 +0100

    update

--- js/plugins.js
+++ js/plugins.js
@@ -25,3 +25,61 @@ btn.on('click', function(e) {
 
 /* event */
 
+window.onscroll = () =>{
+  console.log(window.scrollY+ window.innerHeight);}
+/*
+window.onscroll = () =>{
+  console.log(window.screenX+ window.innerWidth);
+}*/
+myClass = document.getElementsByClassName("more");
+const EventScroll = () =>{
+
+}
+const myScrollFunc = function() {
+let y = window.scrollY, x = window.screenX + window.innerWidth;
+
+  switch(true){
+    case ((y >= 1596 && y < 2026) && (x <= 425) || (y >= 2420 && y < 1996) && (x <= 768)):
+      myClass[0].style.height = '100%';
+      myClass[0].children[0].style.opacity = '1';
+      myClass[1].style.height = '0%';
+      myClass[1].children[0].style.opacity = '0';
+      myClass[2].style.height = '0%';
+      myClass[2].children[0].style.opacity = '0';
+      myClass[3].style.height = '0%';
+      myClass[3].children[0].style.opacity = '0';
+      break;
+    case ((y >= 2026 && y < 2471) && (x <= 425) || (y >= 2420 && y < 1996) && (x <= 768)):
+      myClass[0].style.height = '0%';
+      myClass[0].children[0].style.opacity = '0';
+      myClass[1].style.height = '100%';
+      myClass[1].children[0].style.opacity = '1';
+      myClass[2].style.height = '0%';
+      myClass[2].children[0].style.opacity = '0';
+      myClass[3].style.height = '0%';
+      myClass[3].children[0].style.opacity = '0';
+    break; 
+    case ((y >= 2471 && y < 2901) && (x <= 425) || (y >= 2821 && y < 2420 ) && (x <= 768)):
+      myClass[0].style.height = '0%';
+      myClass[0].children[0].style.opacity = '0';
+      myClass[1].style.height = '0%';
+      myClass[1].children[0].style.opacity = '0';
+      myClass[2].style.height = '100%';
+      myClass[2].children[0].style.opacity = '1';
+      myClass[3].style.height = '0%';
+      myClass[3].children[0].style.opacity = '0';
+    break; 
+    case ((y >= 2901 && x <= 425) || (y >= 2821 && y < 2420) && (x <= 768)):
+      myClass[0].style.height = '0%';
+      myClass[0].children[0].style.opacity = '0';
+      myClass[1].style.height = '0%';
+      myClass[1].children[0].style.opacity = '0';
+      myClass[2].style.height = '0%';
+      myClass[2].children[0].style.opacity = '0';
+      myClass[3].style.height = '100%';
+      myClass[3].children[0].style.opacity = '1';
+    break; 
+    default:
+  }
+} 
+window.addEventListener("scroll", myScrollFunc);
